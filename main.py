29f4171913c7907c52d5567d072f87f601b8d888
Kinda <lamkinun@gmail.com>
2019-12-04 11:58:57 +0800

    demo

--- main.py
+++ main.py
@@ -16,32 +16,8 @@ c.run_video()
 print("takeoff")
 c.drone.do_takeoff()
 
-print("back")
-c.drone.do_back(20)
-
-print("up")
-c.drone.do_up(30)
-
-print("down")
-c.drone.do_down(30)
-
-print("left")
-c.drone.do_left(20)
-
-print("right")
-c.drone.do_right(20)
-
-print("forward")
-c.drone.do_forward(20)
-
-print("cw")
-c.drone.do_cw(90)
-
-print("ccw")
-c.drone.do_ccw(90)
-
-print("go")
-c.drone.do_go(0, 0, 50, 50)
+c.drone.get_tof()
+print(c.drone.response)
 
 print("land")
 c.drone.do_land()
