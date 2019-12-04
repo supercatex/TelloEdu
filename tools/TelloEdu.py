7cc27b1711dff7b19811a2d31efc7c0f464bd6ae
Kinda <lamkinun@gmail.com>
2019-12-04 11:58:44 +0800

    changed getter methods default value to wait response.

--- tools/TelloEdu.py
+++ tools/TelloEdu.py
@@ -180,65 +180,65 @@ class TelloEdu:
     def do_ap(self, ssid, pswd):  # Change to station mode and connect to a new access point.
         self.send_command("ap %s %s" % (ssid, pswd))
 
-    def get_speed(self, wait=False, timeout=3):
+    def get_speed(self, wait=True, timeout=3):
         self.send_command("speed?", wait, timeout)
 
-    def get_battery(self, wait=False, timeout=3):
+    def get_battery(self, wait=True, timeout=3):
         self.send_command("battery?", wait, timeout)
 
-    def get_time(self, wait=False, timeout=3):
+    def get_time(self, wait=True, timeout=3):
         self.send_command("time?", wait, timeout)
 
-    def get_wifi(self, wait=False, timeout=3):
+    def get_wifi(self, wait=True, timeout=3):
         self.send_command("wifi?", wait, timeout)
 
-    def get_sdk(self, wait=False, timeout=3):
+    def get_sdk(self, wait=True, timeout=3):
         self.send_command("sdk?", wait, timeout)
 
-    def get_sn(self, wait=False, timeout=3):
+    def get_sn(self, wait=True, timeout=3):
         self.send_command("sn?", wait, timeout)
 
-    def get_pitch(self, wait=False, timeout=3):
+    def get_pitch(self, wait=True, timeout=3):
         self.send_command("pitch?", wait, timeout)
 
-    def get_roll(self, wait=False, timeout=3):
+    def get_roll(self, wait=True, timeout=3):
         self.send_command("roll?", wait, timeout)
 
-    def get_yaw(self, wait=False, timeout=3):
+    def get_yaw(self, wait=True, timeout=3):
         self.send_command("yaw?", wait, timeout)
 
-    def get_vgx(self, wait=False, timeout=3):  # The speed of "x" axis.
+    def get_vgx(self, wait=True, timeout=3):  # The speed of "x" axis.
         self.send_command("vgx?", wait, timeout)
 
-    def get_vgy(self, wait=False, timeout=3):  # The speed of "y" axis.
+    def get_vgy(self, wait=True, timeout=3):  # The speed of "y" axis.
         self.send_command("vgy?", wait, timeout)
 
-    def get_vgz(self, wait=False, timeout=3):  # The speed of "z" axis.
+    def get_vgz(self, wait=True, timeout=3):  # The speed of "z" axis.
         self.send_command("vgz?", wait, timeout)
 
-    def get_templ(self, wait=False, timeout=3):  # The lowest temperature in degree Celsius.
+    def get_templ(self, wait=True, timeout=3):  # The lowest temperature in degree Celsius.
         self.send_command("templ?", wait, timeout)
 
-    def get_temph(self, wait=False, timeout=3):  # The highest temperature in degree Celsius.
+    def get_temph(self, wait=True, timeout=3):  # The highest temperature in degree Celsius.
         self.send_command("temph?", wait, timeout)
 
-    def get_tof(self, wait=False, timeout=3):  # The time of flight distance in cm.
+    def get_tof(self, wait=True, timeout=3):  # The time of flight distance in cm.
         self.send_command("tof?", wait, timeout)
 
-    def get_h(self, wait=False, timeout=3):  # The height in cm.
+    def get_h(self, wait=True, timeout=3):  # The height in cm.
         self.send_command("h?", wait, timeout)
 
-    def get_bat(self, wait=False, timeout=3):
+    def get_bat(self, wait=True, timeout=3):
         self.send_command("bat?", wait, timeout)
 
-    def get_baro(self, wait=False, timeout=3):  # The barometer measurement in cm.
+    def get_baro(self, wait=True, timeout=3):  # The barometer measurement in cm.
         self.send_command("baro?", wait, timeout)
 
-    def get_agx(self, wait=False, timeout=3):  # The acceleration of the "x" axis.
+    def get_agx(self, wait=True, timeout=3):  # The acceleration of the "x" axis.
         self.send_command("agx?", wait, timeout)
 
-    def get_agy(self, wait=False, timeout=3):  # The acceleration of the "y" axis.
+    def get_agy(self, wait=True, timeout=3):  # The acceleration of the "y" axis.
         self.send_command("agy?", wait, timeout)
 
-    def get_agz(self, wait=False, timeout=3):  # The acceleration of the "z" axis.
+    def get_agz(self, wait=True, timeout=3):  # The acceleration of the "z" axis.
         self.send_command("agz?", wait, timeout)
